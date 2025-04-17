d24748f766aa1446f353391f746e6eb8d572883a
Kyle Sayers <kylesayrs@gmail.com>
2025-04-17 17:40:00 -0400

    add global access
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- src/pttp/profiler.py
+++ src/pttp/profiler.py
@@ -1,4 +1,4 @@
-from typing import List, Set, Dict, Optional
+from typing import List, Set, Dict, Tuple
 
 import gc
 import weakref
@@ -12,6 +12,7 @@ from torch.utils._python_dispatch import TorchDispatchMode
 import matplotlib.pyplot as plt
 
 from .helpers import get_alloc_size
+from .global_access import GlobalAccess
 
 __all__ = ["TensorProfiler"]
 
@@ -24,14 +25,15 @@ class MemoryProfile:
     timeline: List[int] = field(default_factory=list)
 
 
-class TensorProfiler(TorchDispatchMode):
-
+class TensorProfiler(TorchDispatchMode, GlobalAccess):
     _memory = Dict[torch.device, MemoryProfile]
     _tracked_tensors: Set[int]
+    _events = List[Tuple[int, str]]
 
     def __init__(self):
         self._memory: Dict[torch.device, MemoryProfile] = defaultdict(MemoryProfile)
         self._tracked: Set[int] = set()
+        _events: List[Tuple[int, str]] = list()
 
     def __torch_dispatch__(self, func, types, args, kwargs=None):
         ret = func(*args, **(kwargs or {}))
@@ -43,7 +45,7 @@ class TensorProfiler(TorchDispatchMode):
             # aten.copy_.default
             # aten.add_.Tensor
             if tensor_hash in self._tracked:
-                warnings.warn(f"Attmepted to track tensor twice from dispatch {func}")
+                warnings.warn(f"Attempted to track tensor twice from dispatch {func}")
             
             self.track_tensor(ret)
 
@@ -102,11 +104,21 @@ class TensorProfiler(TorchDispatchMode):
 
     ## Plotting
 
+    def mark_event(self, name: str):
+        self._snapshot_timeline()
+        timeline_lens = set(len(timeline) for timeline in self._memory.values())
+        assert len(timeline_lens) == 1
+        self._events.append(timeline_lens[0], name)
+
     def save_memory_profile(self, save_path: str):
         plt.figure()
+
         for device, mem in self._memory.items():
-            label = str(device)
-            plt.plot(mem.timeline, label=label)
+            plt.plot(mem.timeline, label=str(device))
+
+        for index, name in self._events:
+            plt.axvline(x=index, color="gray", linestyle="--", linewidth=1)
+            plt.text(index, plt.ylim()[1] * 0.95, name, rotation=90, verticalalignment="top", fontsize=8)
 
         plt.xlabel("Operation Index")
         plt.ylabel("Memory Usage (bytes)")
---
+++ tests/test_global_access.py
@@ -0,0 +1,10 @@
+from pttp import TensorProfiler
+
+def test_instance():
+    instance = TensorProfiler()
+    assert TensorProfiler.instance() is instance
+
+def test_instance_context():
+    with TensorProfiler() as prof:
+        assert TensorProfiler.instance() is prof
+        breakpoint()
